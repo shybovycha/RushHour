96ede62b377e7240f83c0edb5cbc44e83b3f7920
Damian Gałka <galka.damian.91@gmail.com>
2016-04-07 17:11:30 +0200

    loading file content from html form

--- rush-hour-webapp/src/main/java/pl/edu/uj/ii/webapp/StartApp.java
+++ rush-hour-webapp/src/main/java/pl/edu/uj/ii/webapp/StartApp.java
@@ -7,10 +7,16 @@ import spark.ModelAndView;
 import spark.Request;
 import spark.template.velocity.VelocityTemplateEngine;
 
+import javax.servlet.MultipartConfigElement;
+import javax.servlet.ServletException;
+import javax.servlet.http.Part;
+import java.io.BufferedReader;
 import java.io.IOException;
-import java.util.HashMap;
+import java.io.InputStreamReader;
 import java.util.Map;
 
+import static org.apache.commons.lang.StringUtils.isEmpty;
+import static pl.edu.uj.ii.webapp.execute.SupportedLang.JAVA_8;
 import static spark.Spark.get;
 import static spark.Spark.post;
 
@@ -18,24 +24,51 @@ import static spark.Spark.post;
  * Created by gauee on 4/7/16.
  */
 public class StartApp {
+    public static final String PARAM_SUPPORTED_LANG = "supportedLang";
+    public static final String PARAM_FILE_CONTENT = "fileContent";
     private static final Logger LOGGER = Logger.getLogger(StartApp.class);
 
     public static void main(String[] args) throws IOException {
         get("/", (req, res) -> uploadPageView(), new VelocityTemplateEngine());
-        post("/submit", (req, res) -> processNewSolution(req));
+        post("/submit", "multipart/form-data", (req, res) -> processNewSolution(req), new VelocityTemplateEngine());
     }
 
-    private static Object processNewSolution(Request req) {
-        LOGGER.debug("Request params: " + queryMapOfParams(req));
-        return "Processing solution";
+    private static ModelAndView processNewSolution(Request req) {
+        SupportedLang supportedLang = retrieveSupportedLang(req);
+        String sourceCode = retrieveSourceCode(req);
+        LOGGER.debug("Request params: " + supportedLang + ", SourceCode=" + sourceCode);
+
+        ModelAndView modelAndView = uploadPageView();
+        ((Map<String, Object>) modelAndView.getModel()).put("message", "File uploaded.");
+        return modelAndView;
     }
 
-    private static Map<String, String> queryMapOfParams(Request req) {
-        HashMap<String, String> mapOfParams = Maps.newHashMap();
-        for (String param : req.queryParams()) {
-            mapOfParams.put(param, req.queryParams(param));
+    private static String retrieveSourceCode(Request req) {
+        MultipartConfigElement multipartConfigElement = new MultipartConfigElement("/tmp");
+        req.raw().setAttribute("org.eclipse.jetty.multipartConfig", multipartConfigElement);
+        StringBuilder sourceCodeBuilder = new StringBuilder();
+        try {
+            Part part = req.raw().getPart(PARAM_FILE_CONTENT);
+            try (BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(part.getInputStream()))) {
+                String line;
+                while ((line = bufferedReader.readLine()) != null) {
+                    sourceCodeBuilder.append(line);
+                }
+            }
+        } catch (ServletException | IOException e) {
+            LOGGER.info("Cannot load file.", e);
         }
-        return mapOfParams;
+        return sourceCodeBuilder.toString();
+    }
+
+    private static SupportedLang retrieveSupportedLang(Request req) {
+        String langIdx = req.queryParams(PARAM_SUPPORTED_LANG);
+        if (isEmpty(langIdx)) {
+            return JAVA_8;
+        }
+        int idx = Integer.valueOf(langIdx);
+        SupportedLang[] values = SupportedLang.values();
+        return idx >= 0 && idx < values.length ? values[idx] : JAVA_8;
     }
 
     private static ModelAndView uploadPageView() {
